01999f72c450ecea9cffa9602fe7d6270ea070bc
Shyju Adumkudi <shyju.adumkudi@gmail.com>
2022-01-05 16:50:33 +0530

    Create encrypt.py

---
+++ Python/encrypt.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+ 
+# we will be encryting the below string.
+message = "hello world"
+key = Fernet.generate_key()
+fernet = Fernet(key)
+encMessage = fernet.encrypt(message.encode())
+ 
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+
+decMessage = fernet.decrypt(encMessage).decode() 
+print("decrypted string: ", decMessage)
